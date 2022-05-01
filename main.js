9600872b5385de0e149167d6c26d5a399aeb275a
Onda-dev <manuelaltieri3@gmail.com>
2022-05-01 22:04:54 +0200

    Open ok

---
+++ main.js
@@ -0,0 +1,6 @@
+const hamburgerMenu = document.getElementById("open-menu");
+hamburgerMenu.addEventListener("click",
+    function(){
+        document.getElementById("hamburger-menu").classList.add("display-block")
+    }
+)
